dae2eac0649f01ef876787f1dba7ca6499e582b6
Nicstim <56487341+nicstim@users.noreply.github.com>
2021-08-08 13:50:36 +0300

    Update #1
    update imagefield
    add verbose name in apps config
    add 404.html
    update SectionView

--- section/views.py
+++ section/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import TemplateView
 
-# Create your views here.
+from config.views import common_context
+from .models import *
+
+
+class SectionView(TemplateView):
+    section = None
+
+    def get(self, request, *args, **kwargs):
+        try:
+            self.section = Section.objects.get(slug=kwargs.get("slug"), is_active=True)
+        except Section.DoesNotExist:
+            raise Http404
+        except Section.MultipleObjectsReturned:
+            self.section = Section.objects.filter(slug=kwargs.get("slug"), is_active=True).first()
+
+        return super(SectionView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(SectionView, self).get_context_data(**kwargs)
+        context.update(common_context())
+        context['section'] = self.section
+
+        return context
